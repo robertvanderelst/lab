b48fdc9813534eea231c7619496d81ce57233c91
Robert van der Elst <mail@robertvanderelst.nl>
2021-01-07 11:10:36 +0100

    Update

--- pwa/js/sw.js
+++ pwa/js/sw.js
@@ -1,9 +1,8 @@
 var cacheName = 'hello-pwa';
 var filesToCache = [
-  '/',
-  '/index.html',
-  '/css/style.css',
-  '/js/main.js'
+  'https://lab.robertvanderelst.nl/index.html',
+  'https://lab.robertvanderelst.nl/css/style.css',
+  'https://lab.robertvanderelst.nl/js/main.js'
 ];
 
 /* Start the service worker and cache all of the app's content */
